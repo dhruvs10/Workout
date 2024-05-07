ad09abfa6d1336fefe9fc7a8658e6dedee2d0e3e
dhruvs10 <90457753+dhruvs10@users.noreply.github.com>
2024-05-07 18:14:25 -0400

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms 
-
-from .models import Workout
-
-class WorkoutForm(forms.ModelForm):
-
-    class Meta:
-        model = Workout
-        fields = ['date', 'user', 'notes']
